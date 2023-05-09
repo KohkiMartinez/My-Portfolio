e47f6c876da3043ce911262098a916c329fd2011
Kohki-Martinez <kohki.au.bkb@gmail.com>
2023-05-09 16:20:04 +1000

    Organized all the files

--- EvaluateNewsApp/__test__/urlChecker.test.js
+++
@@ -1,7 +0,0 @@
-import { checkForUrl } from "../src/client/js/urlChecker";
-
-describe("Testing the URL Checker functionality", () => {
-  test("Testing the URL Checker() function", () => {
-    expect(checkForUrl).toBeDefined();
-  });
-});
