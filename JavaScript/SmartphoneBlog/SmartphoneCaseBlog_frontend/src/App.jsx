8816eee33a671160b3353d47fc1e780fecd314d3
KohkiMartinez <106417907+KohkiMartinez@users.noreply.github.com>
2024-05-02 16:44:57 +0900

    added ts

--- JavaScript/SmartphoneBlog/SmartphoneCaseBlog_frontend/src/App.jsx
+++ JavaScript/SmartphoneBlog/SmartphoneCaseBlog_frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes, BrowserRouter } from 'react-router-dom';
 
 import ReadAll from './pages/item/readAll';
 import ReadSingle from './pages/item/readSingle';
+import MuiP2 from './pages/MUI Practice/PractiseMui2'
 
 import './App.css'
 
@@ -13,6 +14,8 @@ function App() {
       <Routes>
         <Route path='/' element={<ReadAll /> } />
         <Route path='/item/:id' element={<ReadSingle />} />
+        <Route path='/muiP2' element={<MuiP2 />} />
+        
       </Routes>
     </BrowserRouter>
   )
--- JavaScript/SmartphoneBlog/SmartphoneCaseBlog_frontend/src/main.jsx
+++ JavaScript/SmartphoneBlog/SmartphoneCaseBlog_frontend/src/main.jsx
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { ThemeProvider } from '@emotion/react'
+import theme from './pages/MUI Practice/theme';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
     <App />
+    </ThemeProvider>
+    
   </React.StrictMode>,
 )
